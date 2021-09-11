cf0dfc82f78f85e753287887082140a227f74469
kopalgarg <kopalgarg24@gmail.com>
2021-09-10 23:54:14 -0400

    Update removeDuplicates.py

--- arrays-101/removeDuplicates.py
+++ arrays-101/removeDuplicates.py
@@ -4,14 +4,10 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        unique = 1
-        i = 1
-        while i > 0 and i < len(nums):
-            print(nums[:i])
-            if nums[i] in nums[:i]:
-                nums.pop(i)
-                nums.append('_')
-                i+=1
-            else:
-                unique+=1
-                i+=1
+        pointer = 1
+        for i in range(len(nums)-1):
+            if nums[i] != nums[i+1]:
+                nums[pointer] = nums[i+1]
+                pointer +=1
+        return pointer
+                
